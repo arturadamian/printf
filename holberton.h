adcbb8382c00783e0c5df80477f8816ed217c04d
Isaiah Becker-Mayer <ibeckermayer@gmail.com>
2018-02-24 22:01:33 +0000

    committing some of the basic functions we'll for sure need, as well as a skeleton .h file and a gitignore

---
+++ holberton.h
@@ -0,0 +1,9 @@
+#ifndef HOLBERTON_H
+#define HOLBERTON_H
+#include <unistd.h>
+#include <stdlib.h>
+#include <stdarg.h>
+int _putchar(char c);
+int _strlen(char *s);
+int _printf(const char *format, ...);
+#endif
