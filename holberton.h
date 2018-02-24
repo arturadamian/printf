1e7b147d263d41bdfb666aad5ccf7e0a4d9c4443
Isaiah Becker-Mayer <ibeckermayer@gmail.com>
2018-02-24 22:38:22 +0000

    defined a structure to contain operation specifiers and their respective functions

--- holberton.h
+++ holberton.h
@@ -6,4 +6,23 @@
 int _putchar(char c);
 int _strlen(char *s);
 int _printf(const char *format, ...);
+
+/**
+ * op_func - Typedef for operator function
+ * @op_func: betty thinks this is a function
+ * Return: for some reason betty wants this
+ */
+typedef	void (*op_func)(void *p);
+
+/**
+ * struct op - Struct op
+ *
+ * @op: The operator
+ * @f: The function associated
+ */
+typedef struct op
+{
+	char *op;
+	op_func f;
+} op_t;
 #endif
