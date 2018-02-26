30b44bd1b0aa6c14fc5d90a6a7d3d80d234ea0c4
Isaiah Becker-Mayer <ibeckermayer@gmail.com>
2018-02-26 06:25:09 +0000

    no, now I fixed intricacy of % operator

--- _printf.c
+++ _printf.c
@@ -50,7 +50,7 @@ int _printf(const char *format, ...)
 			{
 				_putchar('%');
 				chars_printed++;
-				if (op_str[0] == ' ')
+				if (op_str[0] == ' ' && _strcmp(os_temp, "%") != 0)
 					_putchar(' ');
 				if (_strcmp(os_temp, "%") != 0)
 					chars_printed += _putsnnl(skip_lead_sp(op_str));
