bf470a3753a3d43d2d99c0a0066b49aacce2e945
Isaiah Becker-Mayer <ibeckermayer@gmail.com>
2018-02-26 02:27:23 +0000

    changed tests and found some issues

--- test/0-string-main.c
+++ test/0-string-main.c
@@ -15,10 +15,10 @@ int main(void)
 	/* int len4 = printf("%c", '\n'); */
 	/* _printf("Length:[%d, %d, %d, %d]\n", len, len2, len3, len4); */
 
-	int len = _printf(NULL);
-	int len2 = printf(NULL);
-	int len3 = _printf(NULL, '\n');
-	int len4 = printf(NULL, '\n');
+	int len = _printf("%");
+	int len2 = printf("%");
+	int len3 = _printf("%%");
+	int len4 = printf("%%");
 	_printf("Length:[%d, %d, %d, %d]\n", len, len2, len3, len4);
 
 	return (0);
